db587c1cc132645bdb3a2813478d60191a17b9e0
dfuenning <dfuenning@gmail.com>
2020-05-15 16:56:57 -0400

    updating seeds

--- db/schema.sql
+++ db/schema.sql
@@ -13,9 +13,3 @@ CREATE TABLE burgers (
     devoured BOOL NOT NULL
     
 );
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Galley Boy', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Baco Cheeseburger-For a Cop', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Quesadilla Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger', FALSE);
